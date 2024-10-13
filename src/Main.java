2b0d1e6fd00bee24122581acbdab7aadb4a37034
Egurazdov <egurazdov@yandex.ru>
2024-10-13 13:52:53 +0300

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashSet<String> setWatercolor = new HashSet<>();
+        HashSet<String> setGouache = new HashSet<>();
+        setGouache.add("Жёлтый");
+        setGouache.add("Синий");
+        setGouache.add("Красный");
+        setGouache.add("Зелёный");
+        setWatercolor.add("Небесный");
+        setWatercolor.add("Синий");
+        setWatercolor.add("Голубой");
+        setWatercolor.add("Зелёный");
+
+        System.out.println(setWatercolor);
+        System.out.println(setGouache);
+        System.out.println(setSame(setWatercolor, setGouache));
+        System.out.println(setWatercolor);
+        System.out.println(setGouache);
+    }
+
+    private static HashSet<String> setSame(HashSet<String> set1, HashSet<String> set2) {
+        HashSet<String> setTemp = new HashSet<>(set1);
+        setTemp.retainAll(set2);
+        return setTemp;
     }
 }
